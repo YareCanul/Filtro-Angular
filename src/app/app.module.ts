11c53060a11bee85e797224e6d523b4c27edb6dd
Yare Canul <a.yare.canul@utponiente.edu.mx>
2023-02-17 15:31:27 -0600

    actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostComponent } from './components/post/post.component';
 import { FiltroComponent } from './eco/filtro/filtro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
     FiltroComponent
   ],
   imports: [
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
-
+export class implemented{
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
