cffc4f0a8a7b8001178e2ed43422bd8e0c6fca67
lizbethchac1801 <a.lizbeth.chac@utponiente.edu.mx>
2023-02-17 18:38:58 -0600

    Navbar-ecommerce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FiltroComponent } from './eco/filtro/filtro.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FiltroComponent
+    FiltroComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
